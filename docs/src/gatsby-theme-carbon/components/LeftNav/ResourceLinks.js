5b09d984ce01c0d4c8081c5ed37ad19ba74279ac
Daniel Waddington <daniel.waddington@ibm.com>
2020-09-11 14:46:36 -0700

    added gatbsy pages

---
+++ docs/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLinks';
+
+const links = [
+  {
+    title: 'Github',
+    href: 'https://github.com/IBM/mcas/',
+  },
+  {
+    title: 'Documentation',
+    href: 'https://github.com/IBM/mcas/blob/master/info/index.md',
+  },
+];
+
+// shouldOpenNewTabs: true if outbound links should open in a new tab
+const CustomResources = () => <ResourceLinks shouldOpenNewTabs links={links} />;
+
+export default CustomResources;
