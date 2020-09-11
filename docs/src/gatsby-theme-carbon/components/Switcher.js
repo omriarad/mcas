ab449fc66fcedc2d8799ff67b43c5ac8ba4fb223
Daniel Waddington <daniel.waddington@ibm.com>
2020-09-11 16:13:47 -0700

    fixed switcher

--- docs/src/gatsby-theme-carbon/components/Switcher.js
+++ docs/src/gatsby-theme-carbon/components/Switcher.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Switcher from 'gatsby-theme-carbon/src/components/Switcher';
 import { SwitcherLink } from 'gatsby-theme-carbon/src/components/Switcher';
-import { SwitcherDivider } from 'gatsby-theme-carbon/src/components/Switcher';
-
-const switcher_links = [
-  { href: 'https://www.ibm.com/design/', linkText: 'IBM Design ??' },
-];
+//import { SwitcherDivider } from 'gatsby-theme-carbon/src/components/Switcher';
 
 const CustomSwitcher = (props) => (
   <Switcher {...props}>
-    <SwitcherLink href="https://www.research.ibm.com/labs/almaden/">IBM Research Almaden</SwitcherLink>
-    <SwitcherLink href="https://www.ibm.com/design/">IBM Design</SwitcherLink>
+    <SwitcherLink href="https://www.research.ibm.com/labs/almaden/">IBM Research Almaden</SwitcherLink> {}
+    <SwitcherLink href="https://www.ibm.com/design/">IBM Design</SwitcherLink> {}
   </Switcher>
 );
-
-//<SwitcherDivider>Technology Stack</SwitcherDivider>
 
 export default CustomSwitcher;
