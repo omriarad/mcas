537e67c58a6a45e3db1f1157deaf625d0ff87f33
Daniel Waddington <daniel.waddington@ibm.com>
2020-12-18 17:44:00 -0800

    wip

--- src/rust/mcasapi/src/lib.rs
+++ src/rust/mcasapi/src/lib.rs
@@ -1,7 +1,25 @@
+#[allow(dead_code)]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+mod mcasapi_wrapper;
+
+
 #[cfg(test)]
 mod tests {
+
+    use crate::mcasapi_wrapper::{mcas_open_session_ex, mcas_close_session};
+    use std::ffi::CString;
+     
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn open_session() {
+        let ip = CString::new("10.0.0.101::11911").expect("bad string").as_ptr();
+        let device = CString::new("mlx5_0").expect("bad string").as_ptr();
+        let session = unsafe { mcas_open_session_ex(ip, device, 3, 30) };
+
+        println!("session opened");
+        unsafe { mcas_close_session(session) };
+        println!("session closed");
+            
     }
 }
