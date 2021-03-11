84b6b3d9c2759656522db93f1a939fb3fd66fc80
Clem Dickey <dickeycl@us.ibm.com>
2021-03-11 11:45:11 -0800

    spelling fixes

--- src/server/mcas/mcas-logged.sh
+++ src/server/mcas/mcas-logged.sh
@@ -5,20 +5,20 @@
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 LOG=$1; shift
-# Thsi scripts spedifies two kinds of profiling:
+# This script specifies two kinds of profiling:
 #
 # 1. gperftools profiling:
-#   
+#
 #   Compile: run cmake with -DPROFILE=1
 #   Runtime: pass a --profile <file> argument to mcas
-# 
+#
 #   Perhaps -DPROFILE-1 ought to be the default if libprofile is found.
 #   I don't know of any penalty to linking libprofile.
 #
 # 3. Invasive profiling
 #
-#   Compile: run cmake with -DINVASIVE_PERFORMANCE_REPORT=1
-#   Runtime: set env variable MCAS_DURATION_CLOCK_ENABLED=1 
+#   Compile: run cmake with -DINVASIVE_PER=1
+#   Runtime: set env variable MCAS_DURATION_CLOCK_ENABLED=1
 #
 #   Both the compile and runtime enablements will incur a runtime performance penalty.
 #
