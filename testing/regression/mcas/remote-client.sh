57cbae892f62ce97f22475d2ff32352e7eea1386
Clem Dickey <dickeycl@us.ibm.com>
2021-04-19 19:41:47 -0700

    testing: better support for multiple, remote clients

--- testing/regression/mcas/remote-client.sh
+++ testing/regression/mcas/remote-client.sh
@@ -7,7 +7,21 @@ DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 NODE_IP="$(node_ip)"
 DEBUG=${DEBUG:-0}
 
+# replace value of --cores with a value clamped at the actual number of cores
+declare -a args
+while [ 0 -ne ${#@} ]
+do :
+  args[${#args[@]}]="$1"
+  case $1 in
+    --cores) :
+      shift
+      args[${#args[@]}]="$(clamp_cpu $1)"
+    ;;
+  esac
+  shift
+done
+
 # launch client
 
-[ 0 -lt $DEBUG ] && echo $DIR/../bin/kvstore-perf --src_addr $NODE_IP ${1+"$@"}
-$DIR/../bin/kvstore-perf --src_addr "$NODE_IP" ${1+"$@"}
+[ 0 -lt $DEBUG ] && echo $DIR/../bin/kvstore-perf --src_addr $NODE_IP ${args+"${args[@]}"}
+$DIR/../bin/kvstore-perf --src_addr "$NODE_IP" ${args+"${args[@]}"}
