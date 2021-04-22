79749fb19bd40ff4f86763c96e73c98503c33a9e
Clem Dickey <dickeycl@us.ibm.com>
2021-04-21 20:01:20 -0700

    testing: fix mapstore config generator

--- testing/regression/mcas/mapstore-0.py
+++ testing/regression/mcas/mapstore-0.py
@@ -6,7 +6,7 @@ from shard_protos import shard_proto
 
 class mapstore_0(config_0):
     def __init__(self, addr):
-        config_0.__init__(self, shard_proto(addr, mapstore()))
+        config_0.__init__(self, shard_proto(addr, mapstore()), 1)
 
 from sys import argv
 
