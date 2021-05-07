f2c494a85cf46cf65df5ed9f64d97a6a97dc3886
Clem Dickey <dickeycl@us.ibm.com>
2021-05-07 13:23:34 -0700

    testing: multiple regions in Python config scripts

--- testing/regression/mcas/shard_protos.py
+++ testing/regression/mcas/shard_protos.py
@@ -15,42 +15,55 @@ class shard_proto_ado_mixin(dm):
 
 class shard_proto(dm):
     """ shard prototype """
-    def __init__(self, addr, default_backend):
+    def __init__(self, ipaddr, backend, core=0, port=11911):
+        #
+        # steppable itens
+        #
+        self.ipaddr = ipaddr
+        self.core = core
+        self.port = port
+        self.backend = backend
         dm.__init__(self, {
-            "core" : 0,
-            "addr" : addr,
-            "port" : 11911,
+            "core" : core,
+            "addr" : ipaddr,
+            "port" : port,
         })
-        self.merge(default_backend)
+        self.merge(backend)
+    def step(self,n):
+        return shard_proto(self.ipaddr, self.backend, self.core+n, self.port+n)
 
 class shard_proto_net(dm):
     """ shard prototype with network device """
-    def __init__(self, addr, default_backend, net):
+    def __init__(self, ipaddr, backend, net, core=0, port=11911):
         dm.__init__(self, {
-            "core" : 0,
-            "addr" : addr,
-            "port" : 11911,
+            "core" : core,
+            "addr" : ipaddr,
+            "port" : port,
             "net"  : net
         })
-        self.merge(default_backend)
+        self.merge(backend)
 
 
 class shard_proto_dax(shard_proto):
     """ shard prototype (for dax) """
-    def __init__(self, addr, default_backend, path):
-        shard_proto.__init__(self, addr, default_backend)
-        p = dm({ "addr": 0x9000000000 })
+    def __init__(self, ipaddr, backend, path, memaddr=0x9000000000, core=0, port=11911):
+        shard_proto.__init__(self, ipaddr, backend, core, port)
+        self.path = path
+        self.memaddr = memaddr
+        p = dm({ "addr": memaddr })
         p.merge(path)
         self.merge({"dax_config": [ p.value() ]})
+    def step(self,n):
+        return shard_proto_dax(self.ipaddr, self.backend, self.path.step(n), self.memaddr+n*0x1000000000, self.core+n, self.port+n)
 
 class shard_proto_ado(shard_proto):
     """ shard prototype (for ado) """
-    def __init__(self, addr, default_backend):
-        shard_proto.__init__(self, addr, default_backend)
+    def __init__(self, ipaddr, backend):
+        shard_proto.__init__(self, ipaddr, backend)
         self.merge(shard_proto_ado_mixin())
 
 class shard_proto_dax_ado(shard_proto_dax):
     """ shard prototype (for dax, ado) """
-    def __init__(self, addr, default_backend, path):
-        shard_proto_dax.__init__(self, addr, default_backend, path)
+    def __init__(self, ipaddr, backend, path):
+        shard_proto_dax.__init__(self, ipaddr, backend, path)
         self.merge(shard_proto_ado_mixin())
