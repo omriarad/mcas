c9007034def813d8217ec181efb98a0482bf8998
danielwaddington <daniel.waddington@ibm.com>
2021-07-30 11:54:21 -0700

    added custom numpy install

--- src/python/install-python-deps.sh
+++ src/python/install-python-deps.sh
@@ -4,8 +4,16 @@
 #
 # Python3 should be installed
 #
-pip3 install numpy --user -I
 pip3 install matplotlib --user -I
 pip3 install scikit-image --user -I
 pip3 install torch --user -I
 pip3 install flatbuffers --user -I
+
+# we use a custom version of numpy that allows overloading of memory allocators
+# this is temporary until new features enter the mainline
+
+wget https://github.com/dwaddington/python-wheels/archive/refs/tags/v1.0.tar.gz
+tar -zxvf v1.0.tar.gz
+pip3 install ./python-wheels-1.0/numpy-1.19.6.dev0+78b5f9b-cp36-cp36m-linux_x86_64.whl --user
+rm v1.0.tar.gz
+rm -Rf ./python-wheels-1.0
