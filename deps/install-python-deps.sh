c24d40e0b1bb4cb308966b3f53a7d1c6a4b2a554
Daniel Waddington <daniel.waddington@ibm.com>
2021-09-24 09:11:46 -0700

    added chardet

--- deps/install-python-deps.sh
+++ deps/install-python-deps.sh
@@ -6,19 +6,21 @@
 #
 # Python3 should be installed
 #
-pip3 install matplotlib --user -I
-pip3 install scikit-image --user -I
-pip3 install torch --user -I
-pip3 install torchvision --user -I
-pip3 install flatbuffers --user -I
-pip3 install parallel_sort --user -I
-pip3 install cython --user -I
+PIP=python3 -m pip
+${PIP} install matplotlib --user -I
+${PIP} install scikit-image --user -I
+${PIP} install torch --user -I
+${PIP} install torchvision --user -I
+${PIP} install flatbuffers --user -I
+${PIP} install parallel_sort --user -I
+${PIP} install cython --user -I
+${PIP} install chardet --user -I
 
 # we use a custom version of numpy that allows overloading of memory allocators
 # this is temporary until new features enter the mainline
 
 wget https://github.com/dwaddington/python-wheels/archive/refs/tags/v1.0.tar.gz
 tar -zxvf v1.0.tar.gz
-pip3 install ./python-wheels-1.0/numpy-1.19.6.dev0+78b5f9b-cp36-cp36m-linux_x86_64.whl --user -I
+${PIP} install ./python-wheels-1.0/numpy-1.19.6.dev0+78b5f9b-cp36-cp36m-linux_x86_64.whl --user -I
 rm v1.0.tar.gz
 rm -Rf ./python-wheels-1.0
